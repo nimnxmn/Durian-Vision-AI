725e1e68eebc8fa3a431708dfc414631376fc979
Kantaphat <nim.kantaphat1@gmail.com>
2026-05-17 11:47:48 +0900

    Reduce inference image size to 640px for faster CPU inference

--- apps/api/config.py
+++ apps/api/config.py
@@ -7,6 +7,6 @@ STATIC_DIR = ROOT / "apps" / "web" / "out"
 DEFAULT_CONF = 0.25
 DEFAULT_IOU = 0.5
 MAX_UPLOAD_BYTES = 20 * 1024 * 1024
-MAX_LONG_EDGE = 1280
+MAX_LONG_EDGE = 640
 
 ALLOWED_CONTENT_TYPES = {"image/jpeg", "image/png", "image/jpg"}
